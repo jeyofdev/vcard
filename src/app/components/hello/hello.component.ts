97b50484d206622b93c192820db8d723e126e81f
Jérémy Grégoire <46073105+jeyofdev@users.noreply.github.com>
2025-05-02 14:37:32 +0200

    config project with biomejs, husky and commitlint (#1)
    * config: init husky, commitlint and biome
    
    * config: init paths typescript

---
+++ src/app/components/hello/hello.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+	selector: 'app-hello',
+	imports: [],
+	templateUrl: './hello.component.html',
+	styleUrl: './hello.component.scss',
+})
+export class HelloComponent {
+	constructor() {
+		console.log('hello');
+	}
+}
