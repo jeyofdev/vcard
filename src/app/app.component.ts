f0c07b62b1baf0be865955362d9dca4d29610dba
jeyofdev <jgregoire.dev@gmail.com>
2025-05-02 15:44:47 +0200

    refactor: create component name

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { LayoutGlobalComponent } from '@components/layout/layout-global/layout-global.component';
+import { NameComponent } from '@components/name/name.component';
 
 @Component({
 	selector: 'app-root',
-	imports: [LayoutGlobalComponent],
+	imports: [LayoutGlobalComponent, NameComponent],
 	templateUrl: './app.component.html',
 	styleUrl: './app.component.scss',
 })
---
+++ src/app/components/name/name.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+	selector: 'app-name',
+	imports: [],
+	templateUrl: './name.component.html',
+	styleUrl: './name.component.scss',
+})
+export class NameComponent {}
