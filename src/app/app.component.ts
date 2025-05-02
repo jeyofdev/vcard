bded85c19cd04f22b32cc847d8771b97aa44efc6
jeyofdev <jgregoire.dev@gmail.com>
2025-05-02 16:03:27 +0200

    feat: add component biography

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
+import { BiographyComponent } from '@components/content/biography/biography.component';
+import { NameComponent } from '@components/content/name/name.component';
+import { LayoutContentComponent } from '@components/layout/layout-content/layout-content.component';
 import { LayoutGlobalComponent } from '@components/layout/layout-global/layout-global.component';
-import { NameComponent } from '@components/name/name.component';
 
 @Component({
 	selector: 'app-root',
-	imports: [LayoutGlobalComponent, NameComponent],
+	imports: [LayoutGlobalComponent, LayoutContentComponent, NameComponent, BiographyComponent],
 	templateUrl: './app.component.html',
 	styleUrl: './app.component.scss',
 })
---
+++ src/app/components/content/biography/biography.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+	selector: 'app-biography',
+	imports: [],
+	templateUrl: './biography.component.html',
+	styleUrl: './biography.component.scss',
+})
+export class BiographyComponent {}
