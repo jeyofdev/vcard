11921dba83ed04984277e1aa19a6d8937ca36c83
jeyofdev <jgregoire.dev@gmail.com>
2025-05-04 16:12:28 +0200

    config: init environments

--- src/app/services/json-data.service.ts
+++ src/app/services/json-data.service.ts
@@ -3,6 +3,7 @@ import { Injectable, inject } from '@angular/core';
 import { Data } from '@models/data.model';
 import { HardSkills } from '@models/hard-skills.model';
 import { SoftSkills } from '@models/soft-skills.model';
+import { environment } from 'environments/environment';
 import { Observable, map } from 'rxjs';
 
 @Injectable({
@@ -10,16 +11,23 @@ import { Observable, map } from 'rxjs';
 })
 export class JsonDataService {
 	private _httpClient = inject(HttpClient);
+	private URL_PATH = 'assets/data/data.json';
 
 	public findAllHardSkills(): Observable<HardSkills[]> {
-		return this._httpClient.get<Data>('/assets/data/data.json').pipe(map((datas: Data) => datas.hardSkills));
+		return this._httpClient
+			.get<Data>(environment.baseHref + this.URL_PATH)
+			.pipe(map((datas: Data) => datas.hardSkills));
 	}
 
 	public findAllSoftSkills(): Observable<SoftSkills[]> {
-		return this._httpClient.get<Data>('/assets/data/data.json').pipe(map((datas: Data) => datas.softSkills));
+		return this._httpClient
+			.get<Data>(environment.baseHref + this.URL_PATH)
+			.pipe(map((datas: Data) => datas.softSkills));
 	}
 
 	public findDescription(): Observable<string[]> {
-		return this._httpClient.get<Data>('/assets/data/data.json').pipe(map((datas: Data) => datas.description));
+		return this._httpClient
+			.get<Data>(environment.baseHref + this.URL_PATH)
+			.pipe(map((datas: Data) => datas.description));
 	}
 }
