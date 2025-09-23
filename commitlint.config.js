a6290118bfe1049fbe0069992fe628a0020b92cb
jeyofdev <jgregoire.dev@gmail.com>
2025-09-23 14:51:10 +0200

    data: update description text

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,7 +1,7 @@
 export default {
 	extends: ['@commitlint/config-conventional'],
 	rules: {
-		'type-enum': [2, 'always', ['config', 'feat', 'fix', 'doc', 'style', 'refactor']],
+		'type-enum': [2, 'always', ['config', 'feat', 'fix', 'doc', 'style', 'refactor', 'data']],
 		'subject-case': [2, 'never', ['sentence-case']],
 	},
 };
